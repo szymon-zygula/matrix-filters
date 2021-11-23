914f19850840a8e84bf06b9e5fee2780c16ca819
Szymon Zyguła <szymon@zygula.eu>
2021-11-23 20:45:16 +0100

    Expand the interface

--- matrix-filters/MainWindow.xaml.cs
+++ matrix-filters/MainWindow.xaml.cs
@@ -28,5 +28,63 @@ namespace matrix_filters {
         private void ButtonSaveImage_Click(object sender, RoutedEventArgs e) {
 
         }
+
+        private void SliderBrushRadius_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            if(LabelBrushRadius != null) {
+                LabelBrushRadius.Content = ((int)e.NewValue).ToString();
+            }
+        }
+
+        private void ButtonCleanPolygon_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void CircularBrushFilterArea_Checked(object sender, RoutedEventArgs e) {
+            SliderBrushRadius.IsEnabled = true;
+        }
+
+        private void CircularBrushFilterArea_Unchecked(object sender, RoutedEventArgs e) {
+            SliderBrushRadius.IsEnabled = false;
+        }
+
+        private void PolygonFilterArea_Checked(object sender, RoutedEventArgs e) {
+            ButtonCleanPolygon.IsEnabled = true;
+        }
+
+        private void PolygonFilterArea_Unchecked(object sender, RoutedEventArgs e) {
+            ButtonCleanPolygon.IsEnabled = false;
+        }
+
+        private void RadioIdentity_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RadioBlur_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RadioSharpen_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RadioRelief_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RadioEdgeDetection_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RadioCustom_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void CheckboxAutomaticDivisor_Checked(object sender, RoutedEventArgs e) {
+            TextboxDivisor.IsEnabled = false;
+        }
+
+        private void CheckboxAutomaticDivisor_Unchecked(object sender, RoutedEventArgs e) {
+            TextboxDivisor.IsEnabled = true;
+        }
     }
 }
